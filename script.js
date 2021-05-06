edcd8a776196462a8711da9850ca670975bd5e1c
MmeiyuC <>
2021-05-06 15:52:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let basea
+let baseb
+let height
+
+// This is a code for the button:
+document.getElementById('calculate').addEventListener('click', calculatearea)
+
+function calculatearea () {
+  basea = document.getElementById('basea').value
+  baseb = document.getElementById('baseb').value
+  height = document.getElementById('height').value
+  basea = parseFloat(basea)
+  baseb = parseFloat(baseb)
+  height = parseFloat(height)
+  let area = (basea + baseb) / 2 * height // This is the calculation of the area of a trapezoid
+  alert(area)
+}
